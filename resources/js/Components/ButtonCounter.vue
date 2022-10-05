6fd764824cad0f6c17cd777de6bae99f3381ac9a
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-10-05 14:36:27 +0700

    Setup inertia and vue 3

---
+++ resources/js/Components/ButtonCounter.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="count++">
+    Count: {{ count }}
+  </button>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity"
+const count = ref(0);
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <button-counter />
+  </div>
+</template>
+
+<script setup>
+import ButtonCounter from "../Components/ButtonCounter.vue";
+</script>
