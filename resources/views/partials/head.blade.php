e4fc5e6c2b0113eba83452702dbfd6ecfbac5ec6
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2022-10-15 10:16:06 +0700

    Add admin dashboard

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="admin/assets/css/main/app.css">
+<link rel="stylesheet" href="admin/assets/css/main/app-dark.css">
+<link rel="shortcut icon" href="admin/assets/images/logo/favicon.svg" type="image/x-icon">
+<link rel="shortcut icon" href="admin/assets/images/logo/favicon.png" type="image/png">
+<link rel="stylesheet" href="admin/assets/css/shared/iconly.css">
---
+++ resources/views/partials/script.blade.php
@@ -0,0 +1,5 @@
+<script src="admin/assets/js/bootstrap.js"></script>
+<script src="admin/assets/js/app.js"></script>
+<!-- Need: Apexcharts -->
+<script src="admin/assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="admin/assets/js/pages/dashboard.js"></script>
