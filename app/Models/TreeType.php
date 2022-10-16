2453dcfae36bec09bad1a0c0957ba45c40855b46
farelamo <farizfahmi674@gmail.com>
2022-10-17 05:54:28 +0700

    Change name of Tree Type Model

--- app/Models/TreeType.php
+++ app/Models/TreeType.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TreeTypes extends Model
+class TreeType extends Model
 {
     use HasFactory;
 
