288667b07fac5ac3522793fc4d21d74840a603c3
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2022-10-17 16:25:56 +0700

    Update sidebar, head & dashboard

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -4,30 +4,13 @@
     <div id="main">
       <header/>
       <div class="page-heading">
-        <h3>Profile Statistics</h3>
+        <h3>Dashboard</h3>
       </div>
       <div class="page-content">
         <section class="row">
           <div class="col-12 col-lg-9">
             <div class="row">
-              <div class="col-6 col-lg-3 col-md-6">
-                <div class="card">
-                  <div class="card-body px-4 py-4-5">
-                    <div class="row">
-                      <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                        <div class="stats-icon purple mb-2">
-                          <i class="iconly-boldShow"></i>
-                        </div>
-                      </div>
-                      <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Profile Views</h6>
-                        <h6 class="font-extrabold mb-0">112.000</h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-6 col-lg-3 col-md-6">
+              <div class="col-6 col-lg-4 col-md-6">
                 <div class="card">
                   <div class="card-body px-4 py-4-5">
                     <div class="row">
@@ -37,31 +20,31 @@
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Followers</h6>
-                        <h6 class="font-extrabold mb-0">183.000</h6>
+                        <h6 class="text-muted font-semibold">Jumlah Admin</h6>
+                        <h6 class="font-extrabold mb-0">3</h6>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-6 col-lg-3 col-md-6">
+              <div class="col-6 col-lg-4 col-md-6">
                 <div class="card">
                   <div class="card-body px-4 py-4-5">
                     <div class="row">
                       <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                         <div class="stats-icon green mb-2">
-                          <i class="iconly-boldAdd-User"></i>
+                          <i class="bi bi-tree-fill"></i>
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Following</h6>
-                        <h6 class="font-extrabold mb-0">80.000</h6>
+                        <h6 class="text-muted font-semibold">Jumlah Pohon</h6>
+                        <h6 class="font-extrabold mb-0">80</h6>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-6 col-lg-3 col-md-6">
+              <div class="col-6 col-lg-4 col-md-6">
                 <div class="card">
                   <div class="card-body px-4 py-4-5">
                     <div class="row">
@@ -104,7 +87,7 @@
                           <svg class="bi text-primary" width="32" height="32" fill="blue"
                             style="width:10px">
                             <use
-                              xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                              xlink:href="/admin/assets/images/bootstrap-icons.svg#circle-fill" />
                           </svg>
                           <h5 class="mb-0 ms-3">Europe</h5>
                         </div>
@@ -122,7 +105,7 @@
                           <svg class="bi text-success" width="32" height="32" fill="blue"
                             style="width:10px">
                             <use
-                              xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                              xlink:href="/admin/assets/images/bootstrap-icons.svg#circle-fill" />
                           </svg>
                           <h5 class="mb-0 ms-3">America</h5>
                         </div>
@@ -140,7 +123,7 @@
                           <svg class="bi text-danger" width="32" height="32" fill="blue"
                             style="width:10px">
                             <use
-                              xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                              xlink:href="/admin/assets/images/bootstrap-icons.svg#circle-fill" />
                           </svg>
                           <h5 class="mb-0 ms-3">Indonesia</h5>
                         </div>
@@ -174,7 +157,7 @@
                             <td class="col-3">
                               <div class="d-flex align-items-center">
                                 <div class="avatar avatar-md">
-                                  <img src="assets/images/faces/5.jpg">
+                                  <img src="/admin/assets/images/faces/5.jpg">
                                 </div>
                                 <p class="font-bold ms-3 mb-0">Si Cantik</p>
                               </div>
@@ -187,7 +170,7 @@
                             <td class="col-3">
                               <div class="d-flex align-items-center">
                                 <div class="avatar avatar-md">
-                                  <img src="assets/images/faces/2.jpg">
+                                  <img src="/admin/assets/images/faces/2.jpg">
                                 </div>
                                 <p class="font-bold ms-3 mb-0">Si Ganteng</p>
                               </div>
@@ -211,7 +194,7 @@
               <div class="card-body py-4 px-4">
                 <div class="d-flex align-items-center">
                   <div class="avatar avatar-xl">
-                    <img src="assets/images/faces/1.jpg" alt="Face 1">
+                    <img src="/admin/assets/images/faces/1.jpg" alt="Face 1">
                   </div>
                   <div class="ms-3 name">
                     <h5 class="font-bold">John Duck</h5>
@@ -227,7 +210,7 @@
               <div class="card-content pb-4">
                 <div class="recent-message d-flex px-4 py-3">
                   <div class="avatar avatar-lg">
-                    <img src="assets/images/faces/4.jpg">
+                    <img src="/admin/assets/images/faces/4.jpg">
                   </div>
                   <div class="name ms-4">
                     <h5 class="mb-1">Hank Schrader</h5>
@@ -236,7 +219,7 @@
                 </div>
                 <div class="recent-message d-flex px-4 py-3">
                   <div class="avatar avatar-lg">
-                    <img src="assets/images/faces/5.jpg">
+                    <img src="/admin/assets/images/faces/5.jpg">
                   </div>
                   <div class="name ms-4">
                     <h5 class="mb-1">Dean Winchester</h5>
@@ -245,7 +228,7 @@
                 </div>
                 <div class="recent-message d-flex px-4 py-3">
                   <div class="avatar avatar-lg">
-                    <img src="assets/images/faces/1.jpg">
+                    <img src="/admin/assets/images/faces/1.jpg">
                   </div>
                   <div class="name ms-4">
                     <h5 class="mb-1">John Dodol</h5>
